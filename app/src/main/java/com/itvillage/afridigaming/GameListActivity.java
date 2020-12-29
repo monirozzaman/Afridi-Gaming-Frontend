a34e06d650aac52f2625b3e8bc11a3b2ceb5f2f6
monirozzaman <moniruzzaman35-223@diu.edu.bd>
2020-12-29 17:47:05 +0600

    Add Custom Games List

--- app/src/main/java/com/itvillage/afridigaming/GameListActivity.java
+++ app/src/main/java/com/itvillage/afridigaming/GameListActivity.java
@@ -15,61 +15,62 @@ public class GameListActivity extends AppCompatActivity {
     ListView list;
 
     String[] maintitle ={
-            "Title 1","Title 2",
+            "All Weapons | Mobile Only | 3234","Title 2",
             "Title 3","Title 4",
             "Title 5",
     };
 
     String[] subtitle ={
-            "Sub Title 1","Sub Title 2",
+            "Time: 29/29/2020 at 20:20 ","Sub Title 2",
             "Sub Title 3","Sub Title 4",
             "Sub Title 5",
     };
 
     Integer[] imgid={
-            R.drawable.add_game_icon,R.drawable.add_game_icon,
-            R.drawable.add_game_icon,R.drawable.add_game_icon,
-            R.drawable.add_game_icon
+            R.drawable.free_fire,R.drawable.free_fire,
+            R.drawable.free_fire,R.drawable.free_fire,
+            R.drawable.free_fire
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
+
         GameListAdapter adapter=new GameListAdapter(this, maintitle, subtitle,imgid);
         list=(ListView)findViewById(R.id.game_list);
         list.setAdapter(adapter);
 
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // TODO Auto-generated method stub
-                if(position == 0) {
-                    //code specific to first list item
-                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 1) {
-                    //code specific to 2nd list item
-                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 2) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 3) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 4) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
+//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // TODO Auto-generated method stub
+//                if(position == 0) {
+//                    //code specific to first list item
+//                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+//                }
+//
+//                else if(position == 1) {
+//                    //code specific to 2nd list item
+//                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
+//                }
+//
+//                else if(position == 2) {
+//
+//                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
+//                }
+//                else if(position == 3) {
+//
+//                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
+//                }
+//                else if(position == 4) {
+//
+//                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+//                }
+//
+//            }
+//        });
 
     }
 }
